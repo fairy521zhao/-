5708370e1aca075fabdc12b191381c12f66b9958
fairy521zhao <3796845279@qq.com>
2025-09-09 21:34:20 +0800

    Add files via upload

---
+++ rsa.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+using namespace std;
+
+// 判断一个数是否为质数
+bool isPrime(int num) {
+    if (num < 2) return false;
+    if (num == 2 || num == 3) return true;
+    if (num % 2 == 0) return false;
+    for (int i = 3; i <= sqrt(num); i += 2) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+// 生成指定范围内的随机质数
+int generatePrime(int min, int max) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(min, max);
+    int num;
+    do {
+        num = dis(gen);
+    } while (!isPrime(num));
+    return num;
+}
+
+// 计算最大公约数（欧几里得算法）
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
